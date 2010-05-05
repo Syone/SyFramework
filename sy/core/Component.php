de368093a8c2d0ea7ba4ab43c0a4675ab5471a76
Syone <syone7@gmail.com>
2010-05-05 16:04:37 +0200

    Add namespace.

--- sy/core/Component.php
+++ sy/core/Component.php
@@ -1,4 +1,6 @@
 <?php
+namespace Sy;
+
 /**
  * Description of Component
  *
--- sy/core/WebComponent.php
+++ sy/core/WebComponent.php
@@ -1,8 +1,5 @@
 <?php
-/* 
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+namespace Sy;
 
 /**
  * Description of WebComponent
