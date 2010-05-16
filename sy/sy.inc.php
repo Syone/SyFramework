9414a5903d1eb727ff345173ef53086158cc01f8
syone <syone@localhost>
2010-05-17 01:24:48 +0200

    sy_autoload correction

--- sy/sy.inc.php
+++ sy/sy.inc.php
@@ -3,6 +3,7 @@
 include __DIR__ . '/sy.conf.php';
 
 function sy_autoload($class) {
+	$class = basename($class);
 	if (file_exists($file = __DIR__ . "/core/$class.php")) {
 		require_once $file;
 	}
