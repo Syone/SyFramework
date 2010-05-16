9f9f603c84362f26bf0e8138282887e47954db18
syone <syone@localhost>
2010-05-17 00:26:57 +0200

    Add autoload function

--- sy/sy.inc.php
+++ sy/sy.inc.php
@@ -1,2 +1,11 @@
 <?php
+
 include __DIR__ . '/sy.conf.php';
+
+function sy_autoload($class) {
+	if (file_exists($file = __DIR__ . "/core/$class.php")) {
+		require_once $file;
+	}
+}
+
+spl_autoload_register("sy_autoload");
