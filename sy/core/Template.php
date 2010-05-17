5b9a515bc9450a557cf43c8b9287a38e97d6a345
syone <syone@localhost>
2010-05-17 15:58:01 +0200

    add setFile function

--- sy/core/Template.php
+++ sy/core/Template.php
@@ -3,29 +3,46 @@ namespace Sy;
 
 class Template implements ITemplate {
 
-	public function setRoot($path) {
+	private $root;
 
+	private $content;
+
+	private $vars;
+
+	public function  __construct($root = '.') {
+		$this->setRoot($root);
+		$this->content = '';
+		$this->vars = array();
+	}
+
+	public function setRoot($path) {
+		$this->root = rtrim($path, '/\\');
 	}
 
 	public function setTemplateFile($fileName) {
-
+		$this->content = file_get_contents($this->root . '/' . $fileName);
 	}
 
 	public function setFile($var, $fileName, $append = false) {
-
+		$this->content = str_replace('{' . $var . '}', file_get_contents($this->root . '/' . $fileName), $this->content);
 	}
 
 	public function setVar($var, $value, $append = false) {
-
+		if ($append)
+			$this->vars['{' . $var . '}'] .= $value;
+		else
+			$this->vars['{' . $var . '}'] = $value;
 	}
 
 	public function setBlock($block) {
-
-	}
-
-	public function getRender() {
 		
 	}
 
+	public function getRender() {
+		$res =  str_replace(array_keys($this->vars), array_values($this->vars), $this->content);
+		$res = preg_replace('/{[^ \t\r\n}]+}/', "", $res);
+		return $res;
+	}
+	
 }
 ?>
--- sy/core/TemplateProvider.php
+++ sy/core/TemplateProvider.php
@@ -3,8 +3,14 @@ namespace Sy;
 
 class TemplateProvider {
 
+	/**
+	 * Return a new ITemplate object
+	 *
+	 * @return ITemplate
+	 */
 	public static function createTemplate() {
-		return new PhpLibTemplate();
+		//return new PhpLibTemplate();
+		return new Template();
 	}
 	
 }
