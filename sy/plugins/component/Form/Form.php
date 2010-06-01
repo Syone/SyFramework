ddb50e8f7bab424d9e25c5292983306be1694b04
syone <syone@localhost>
2010-06-01 23:30:25 +0200

    Replace '\' by '/' in autoload functions

--- sy/plugins/component/Form/Form.php
+++ sy/plugins/component/Form/Form.php
@@ -2,6 +2,7 @@
 namespace Sy\Form;
 
 function autoload($class) {
+	$class = str_replace('\\', '/', $class);
 	$class = basename($class);
 	if (file_exists($file = __DIR__ . "/$class.php")) {
 		require_once $file;
--- sy/sy.inc.php
+++ sy/sy.inc.php
@@ -4,6 +4,7 @@ namespace Sy;
 include __DIR__ . '/sy.conf.php';
 
 function autoload($class) {
+	$class = str_replace('\\', '/', $class);
 	$class = basename($class);
 	if (file_exists($file = __DIR__ . "/core/$class.php")) {
 		require_once $file;
