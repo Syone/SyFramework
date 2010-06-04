8e826c81099e68f6f849d33aec64ce615270875f
sye042 <sye042@localhost>
2010-06-04 11:45:41 +0200

    Bug fix and add some features to Query class.

--- sy/core/Query.php
+++ sy/core/Query.php
@@ -2,39 +2,51 @@
 namespace Sy;
 
 class Query {
-	private $DBConnection;
-	private $affectedRows;
-	private $PDOStatement;
-	
-	public function __construct($databaseID) {
-		$this->DBConnection = ConnectionManager::getConnection($databaseID);
-		
-		// FIXME maybe check whether connection is retrieved.
-		
-		$this->affectedRows = 0;
-	}
-	
-	public function execute($query, $params) {
-		$ret = true;
-		
-		try {
-			$this->PDOStatement = $this->DBConnection->prepare($query);
-			$this->PDOStatement->execute($params);
-			
-			$this->affectedRows = $this->PDOStatement->rowCount();
-		} catch (PDOException $e) {
-			$ret = false;
-		}
-		
-		return $ret;
-	}
-	
-	public function fetch() {
-		return $this->PDOStatement->fetch();
-	}
-	
-	public function getStatement() {
-		return $this->PDOStatement;
-	}
+  private $DBConnection;
+  private $PDOStatement;
+
+  public function __construct($databaseID) {
+    $this->DBConnection = ConnectionManager::getConnection($databaseID);
+
+    // FIXME maybe check whether connection is retrieved.
+
+    $this->affectedRows = 0;
+  }
+
+  public function errorInfo() {
+    $infos = $this->PDOStatement->errorInfo();
+
+    if (!isset($infos[0]))
+      return null;
+
+
+    return $infos[2];
+  }
+
+  public function execute($query, $params) {
+    $ret = true;
+
+    try {
+      $this->PDOStatement = $this->DBConnection->prepare($query);
+      $this->PDOStatement->execute($params);
+    } catch (PDOException $e) {
+      $ret = false;
+    }
+
+    return $ret;
+  }
+
+  public function fetch() {
+    return $this->PDOStatement->fetch();
+  }
+
+  public function getStatement() {
+    return $this->PDOStatement;
+  }
+
+  public function rowCount() {
+    return $this->PDOStatement->rowCount();
+  }
 }
+
 ?>
