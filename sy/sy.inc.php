da5c1f7349938a3b4113755812f38ebb3e00670f
syone <syone@localhost>
2010-06-08 09:35:42 +0200

    Remove sy configuration file

--- sy/sy.inc.php
+++ sy/sy.inc.php
@@ -1,15 +1,13 @@
 <?php
 namespace Sy;
 
-include __DIR__ . '/sy.conf.php';
-
 function autoload($class) {
 	$class = str_replace('\\', '/', $class);
 	$class = basename($class);
 	if (file_exists($file = __DIR__ . "/core/$class.php")) {
 		require_once $file;
 	}
-	else if (file_exists($file = __DIR__ . "/../" . COMPONENT_ROOT . "/$class/$class.php")) {
+	else if (file_exists($file = __DIR__ . "/components/$class/$class.php")) {
 		require_once $file;
 	}
 }
