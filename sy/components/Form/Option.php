d697b5b889e4477b292b43a2ea3a835471ab0f87
syone <syone@localhost>
2010-06-14 23:08:45 +0200

    [Form] #3 remove echo message

--- sy/components/Form/Option.php
+++ sy/components/Form/Option.php
@@ -8,9 +8,7 @@ class Option extends Element {
 	}
 
 	public function fill($values) {
-		print_r($values);
 		$v = is_null($this->getAttribute('value')) ? $this->content : $this->getAttribute('value');
-		echo ' v = ' . $v;
 		if (is_array($values)) {
 			if (in_array($v, $values)) $this->setAttribute('selected', 'selected');
 		} else {
