2e9fb1c9cd8bc1b20a60fdfe8dd0e9d5a06946be
syone <syone@localhost>
2010-07-14 15:07:43 +0200

    [Form] #3 add TextElement class, add Validator's methods

--- sy/components/Form/Text.php
+++ sy/components/Form/Text.php
@@ -1,9 +1,7 @@
 <?php
 namespace Sy\Form;
 
-require 'Validator.php';
-
-class Text extends Element {
+class Text extends TextElement {
 
 	public function __construct() {
 		parent::__construct('input');
@@ -14,25 +12,5 @@ class Text extends Element {
 		$this->setAttribute('value', $value);
 	}
 
-	public function isValid($value) {
-		if ($this->isRequired()) {
-			if (!isset($value) or $value === '') return false;
-		} else {
-			if (empty($value)) return true;
-		}
-		if (!isset($this->options['validator'])) return true;
-		$validators = $this->options['validator'];
-		if (!is_array($validators)) $validators = array($validators);
-		foreach ($validators as $v) {
-			$options = array();
-			$filter = FILTER_CALLBACK;
-			$validator = new Validator();
-			if (method_exists($validator, $v)) $options['options'] = array($validator, $v);
-			if (function_exists($v)) $options['options'] = $v;
-			if (empty($options)) continue;
-			if (!filter_var($value, $filter, $options)) return false;
-		}
-		return true;
-	}
 }
 ?>
