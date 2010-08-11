6f6023ffd3b3327633426098f05b204e8088c154
syone <syone@localhost>
2010-08-11 21:03:49 +0200

    Form issue #3 File is a ValidableElement

--- sy/components/Form/File.php
+++ sy/components/Form/File.php
@@ -1,11 +1,21 @@
 <?php
 namespace Sy\Form;
 
-class File extends Element {
+class File extends Element implements ValidableElement {
 	
 	public function __construct() {
 		parent::__construct('input');
 		$this->setAttribute('type', 'file');
+	}
+
+	public function isValid($value) {
+		if ($this->isRequired()) {
+			if (!isset($_FILES[$this->getAttribute('name')])) {
+				$this->error = true;
+				return false;
+			}
+		}
+		return true;
 	}
 
 }
