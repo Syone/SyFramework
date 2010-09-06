c0e3e57223c16568678ec00ac0796ce41a1339f1
syone <syone@localhost>
2010-09-06 23:37:53 +0200

    TemplateProvider: prevent error when template type doesn't exist

--- sy/core/TemplateProvider.php
+++ sy/core/TemplateProvider.php
@@ -9,9 +9,9 @@ class TemplateProvider {
 	 * @return ITemplate
 	 */
 	public static function createTemplate($type = '') {
-		$type = strtolower($type);
-		$type = ucfirst($type);
+		$type = ucfirst(strtolower($type));
 		$class = 'Sy\\' . $type . 'Template';
+		if (!\class_exists($class)) $class = 'Sy\Template';
 		return new $class();
 	}
 	
