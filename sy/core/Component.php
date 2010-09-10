bbe663dda945d227f73a9ac263fb15b128595648
syone <syone@localhost>
2010-09-11 00:37:52 +0200

    [Component] change utility methods implementation

--- sy/core/Component.php
+++ sy/core/Component.php
@@ -107,62 +107,77 @@ class Component {
 
 	/**
 	 * Return the GET parameter named $param
-	 * If the parameter is not set or is empty, return the default value
+	 * If the parameter is not set, return the default value
 	 *
 	 * @param string $param GET parameter name
 	 * @param mixed $default The default value
 	 * @return mixed
 	 */
 	protected function get($param, $default = NULL) {
-		return (isset($_GET[$param]) and !empty($_GET[$param])) ? $_GET[$param] : $default;
+		if (is_null($default))
+			return isset($_GET[$param]) ? $_GET[$param] : $default;
+		else
+			return (isset($_GET[$param]) and !empty($_GET[$param])) ? $_GET[$param] : $default;
 	}
 
 	/**
 	 * Return the POST parameter named $param
-	 * If the parameter is not set or is empty, return the default value
+	 * If the parameter is not set, return the default value
 	 *
 	 * @param string $param POST parameter name
 	 * @param mixed $default The default value
 	 * @return mixed
 	 */
 	protected function post($param, $default = NULL) {
-		return (isset($_POST[$param]) and !empty($_POST[$param])) ? $_POST[$param] : $default;
+		if (is_null($default))
+			return isset($_GET[$param]) ? $_GET[$param] : $default;
+		else
+			return (isset($_POST[$param]) and !empty($_POST[$param])) ? $_POST[$param] : $default;
 	}
 
 	/**
 	 * Return the COOKIE parameter named $param
-	 * If the parameter is not set or is empty, return the default value
+	 * If the parameter is not set, return the default value
 	 *
 	 * @param string $param COOKIE parameter name
 	 * @param mixed $default The default value
 	 * @return mixed
 	 */
 	protected function cookie($param, $default = NULL) {
-		return (isset($_COOKIE[$param]) and !empty($_COOKIE[$param])) ? $_COOKIE[$param] : $default;
+		if (is_null($default))
+			return isset($_COOKIE[$param]) ? $_COOKIE[$param] : $default;
+		else
+			return (isset($_COOKIE[$param]) and !empty($_COOKIE[$param])) ? $_COOKIE[$param] : $default;
 	}
 
 	/**
 	 * Return the REQUEST parameter named $param
-	 * If the parameter is not set or is empty, return the default value
+	 * If the parameter is not set, return the default value
 	 *
 	 * @param string $param REQUEST parameter name
 	 * @param mixed $default The default value
 	 * @return mixed
 	 */
 	protected function request($param, $default = NULL) {
-		return (isset($_REQUEST[$param]) and !empty($_REQUEST[$param])) ? $_REQUEST[$param] : $default;
+		if (is_null($default))
+			return isset($_REQUEST[$param]) ? $_REQUEST[$param] : $default;
+		else
+			return (isset($_REQUEST[$param]) and !empty($_REQUEST[$param])) ? $_REQUEST[$param] : $default;
 	}
 
 	/**
 	 * Return the SESSION parameter named $param
-	 * If the parameter is not set or is empty, return the default value
+	 * If the parameter is not set, return the default value
 	 *
 	 * @param string $param SESSION parameter name
 	 * @param mixed $default The default value
 	 * @return mixed
 	 */
 	protected function session($param, $default = NULL) {
-		return (isset($_SESSION[$param]) and !empty($_SESSION[$param])) ? $_SESSION[$param] : $default;
+		if (is_null($default))
+			return isset($_SESSION[$param]) ? $_SESSION[$param] : $default;
+		else
+			return (isset($_SESSION[$param]) and !empty($_SESSION[$param])) ? $_SESSION[$param] : $default;
 	}
 
 	/**
