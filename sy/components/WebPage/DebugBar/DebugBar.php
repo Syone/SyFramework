e8b8c798ae8c05630c7d9d97dd21d242804aa0fd
syone <syone@localhost>
2010-12-30 17:10:09 +0100

    web debug bar

--- sy/components/WebPage/DebugBar/DebugBar.php
+++ sy/components/WebPage/DebugBar/DebugBar.php
@@ -10,16 +10,26 @@ class DebugBar extends WebComponent {
 	}
 
 	private function init() {
+		// PHP Info
+		if (!is_null($this->get('phpinfo'))) {
+			phpinfo(INFO_GENERAL | INFO_CREDITS | INFO_CONFIGURATION | INFO_MODULES | INFO_ENVIRONMENT | INFO_LICENSE);
+			exit();
+		}
+
+		// PHP Variables
 		$constants = get_defined_constants(true);
 		$vars_array['User Constants'] = $constants['user'];
-		$vars_array['_REQUEST'] = $_REQUEST;
-		$vars_array['_GET'] = $_GET;
-		$vars_array['_POST'] = $_POST;
-		$vars_array['_COOKIE'] = $_COOKIE;
-		$vars_array['_FILES'] = $_FILES;
-		$vars_array['_SERVER'] = $_SERVER;
-		$vars_array['_ENV'] = $_ENV;
+		$vars_array['$_REQUEST Variables'] = $_REQUEST;
+		$vars_array['$_GET Variables'] = $_GET;
+		$vars_array['$_POST Variables'] = $_POST;
+		$vars_array['$_COOKIE Variables'] = $_COOKIE;
+		$vars_array['$_FILES Variables'] = $_FILES;
+		$vars_array['$_SERVER Variables'] = $_SERVER;
+		$vars_array['$_ENV Variables'] = $_ENV;
 		$this->setVar('VARS_ARRAY', $vars_array);
+
+		// Files
+		$this->setVar('FILES', get_included_files());
 	}
 
 }
