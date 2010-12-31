1d16fc2d755136fc24138419a5d84ad2dc57e34d
syone <syone@localhost>
2010-12-31 03:34:23 +0100

    web debug bar: log panel

--- sy/core/Debugger.php
+++ sy/core/Debugger.php
@@ -3,10 +3,14 @@ namespace Sy;
 
 class Debugger {
 
-	private static $logs = array();
+	public static $logs = array();
 
 	public static function log($message, $level, $type) {
 		self::$logs[] = new Log($message, $level, $type);
+	}
+
+	public static function getLogs() {
+		return self::$logs;
 	}
 
 }
