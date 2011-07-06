1349cad26b00df065652d80916b539ae2223af46
syone <syone@localhost>
2011-07-06 14:20:44 +0200

    Db correction

--- syf/Sy/Db/Connection.php
+++ syf/Sy/Db/Connection.php
@@ -8,7 +8,7 @@ class Connection {
 	public static function instance($dsn, $username = '', $passwd = '', array $options = array()) {
 		$key = md5($dsn . $username . $passwd . serialize($options));
 		if (!isset(self::$connections[$key]))
-			self::$connections[$key] = new PDO($dsn, $username, $passwd, $options);
+			self::$connections[$key] = new \PDO($dsn, $username, $passwd, $options);
 		return self::$connections[$key];
 	}
 
