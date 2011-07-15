0299a82ed21e95307adc6983fc7fec63b94a0423
syone <syone@localhost>
2011-07-16 01:04:57 +0200

    bug fix: create log file directory if not exist

--- syf/Sy/Debug/FileLogger.php
+++ syf/Sy/Debug/FileLogger.php
@@ -4,13 +4,20 @@ namespace Sy\Debug;
 class FileLogger implements ILogger {
 
 	private $file;
+	private $ttl;
 
-	public function __construct($file) {
+	public function __construct($file, $ttl) {
 		$this->file = $file;
+		$this->ttl = $ttl;
 	}
 
 	public function write(Log $log) {
-		if (file_exists($this->file) and ((time() - filemtime($this->file)) > 90)) file_put_contents($this->file, '', LOCK_EX);
+		if (!file_exists(dirname($this->file))) {
+			mkdir(dirname($this->file), 0777, true);
+		}
+		if (file_exists($this->file) and ((time() - filemtime($this->file)) > $this->ttl)) {
+			file_put_contents($this->file, '', LOCK_EX);
+		}
 		file_put_contents($this->file, $this->formatLog($log), FILE_APPEND | LOCK_EX);
 	}
 
