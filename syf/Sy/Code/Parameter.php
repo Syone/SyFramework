8dcf90172afa404474d31109ff967ba81a2eb5ed
syone <syone@localhost>
2011-11-17 18:00:30 +0100

    add Sy\Code

---
+++ syf/Sy/Code/Parameter.php
@@ -0,0 +1,39 @@
+<?php
+namespace Sy\Code;
+
+use Sy\Component;
+
+class Parameter extends Component {
+
+	private $type;
+	private $name;
+	private $defaultValue;
+
+	public function __construct($name, $defaultValue = NULL) {
+		parent::__construct();
+		$this->setTemplateFile(__DIR__ . '/templates/Parameter.tpl');
+		$this->type = NULL;
+		$this->name = $name;
+		$this->defaultValue = $defaultValue;
+	}
+
+	public function getName() {
+		return $this->name;
+	}
+
+	public function getType() {
+		return $this->type;
+	}
+
+	public function setType($type) {
+		$this->type = $type;
+	}
+
+	public function __toString() {
+		$this->setVar('NAME', $this->name);
+		if (!is_null($this->type)) $this->setVar('TYPE', $type . ' ');
+		if (!is_null($this->defaultValue)) $this->setVar('DEFAULT_VALUE', ' = ' . $this->defaultValue);
+		return parent::__toString();
+	}
+
+}
---
+++ syf/Sy/Code/templates/Method.tpl
@@ -0,0 +1,8 @@
+	/**
+	 *<!-- BEGIN PARAMETERS_BLOCK -->
+	 * @param {PARAM_TYPE} {PARAM_NAME}
+	 <!-- END PARAMETERS_BLOCK -->
+	 */
+	{VISIBILITY}{STATIC} function {NAME}({PARAMETERS}) {
+{BODY}
+	}
