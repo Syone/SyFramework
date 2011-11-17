c226563eda8977e882d4e419024093eba9941675
syone <syone@localhost>
2011-11-17 18:00:30 +0100

    add Sy\Code

---
+++ syf/Sy/Code/Classe.php
@@ -0,0 +1,65 @@
+<?php
+namespace Sy\Code;
+
+use Sy\Component;
+
+class Classe extends Component {
+
+	private $namespace;
+	private $abstract;
+	private $name;
+	private $extendedClass;
+	private $implementedInterfaces;
+    private $properties;
+    private $methods;
+
+	public function __construct($namespace, $name) {
+		parent::__construct();
+		$this->setTemplateFile(__DIR__ . '/templates/Classe.tpl');
+		$this->namespace = $namespace;
+		$this->abstract = false;
+		$this->name = $name;
+		$this->extendedClass = NULL;
+		$this->implementedInterfaces = array();
+		$this->properties = array();
+		$this->methods = array();
+	}
+
+	public function setAbstract($abtract) {
+		$this->abstract = $abtract;
+	}
+
+	public function setExtendedClass($class) {
+		$this->extendedClass = $class;
+	}
+
+	public function setImplementedInterfaces(array $interfaces) {
+		$this->implementedInterfaces = $interfaces;
+	}
+
+	public function setProperties(array $properties) {
+		$this->properties = $properties;
+	}
+
+	public function setMethods(array $methods) {
+		$this->methods = $methods;
+	}
+
+	public function __toString() {
+		$this->setVar('NAMESPACE', $this->namespace);
+		$this->setVar('NAME', $this->name);
+		if ($this->abstract) $this->setVar('ABSTRACT', 'abstract ');
+		if (!is_null($this->extendedClass)) $this->setVar('EXTENDS', ' extends ' . $this->extendedClass);
+		if (!empty($this->implementedInterfaces)) $this->setVar('IMPLEMENTS', ' implements ' . implode(', ', $this->implementedInterfaces));
+		foreach ($this->properties as $property) {
+			$this->setComponent('PROPERTY', $property);
+			$this->setBlock('PROPERTIES');
+		}
+		foreach ($this->methods as $method) {
+			$this->setComponent('METHOD', $method);
+			$this->setBlock('METHODS');
+		}
+		return parent::__toString();
+	}
+
+}
---
+++ syf/Sy/Code/Method.php
@@ -0,0 +1,48 @@
+<?php
+namespace Sy\Code;
+
+use Sy\Component;
+
+class Method extends Component {
+
+	private $visibility;
+	private $static;
+	private $name;
+	private $parameters;
+	private $body;
+
+	public function __construct($visibility, $name, $parameters = array()) {
+		parent::__construct();
+		$this->setTemplateFile(__DIR__ . '/templates/Method.tpl');
+		$this->visibility = $visibility;
+		$this->name = $name;
+		$this->parameters = $parameters;
+		$this->static = false;
+		$this->body = '';
+	}
+
+	public function setStatic($static) {
+		$this->static = $static;
+	}
+
+	public function setBody($body) {
+		$this->body = $body;
+	}
+
+	public function __toString() {
+		$this->setVar('VISIBILITY', $this->visibility);
+		$this->setVar('NAME', $this->name);
+		$this->setVar('BODY', $this->body);
+		if ($this->static) $this->setVar('STATIC', ' static');
+		if (!empty($this->parameters)) {
+			$this->setVar('PARAMETERS', implode(', ', $this->parameters));
+			foreach ($this->parameters as $parameter) {
+				$this->setVar('PARAM_TYPE', is_null($parameter->getType()) ? 'type' : $parameter->getType());
+				$this->setVar('PARAM_NAME', '$' . $parameter->getName());
+				$this->setBlock('PARAMETERS_BLOCK');
+			}
+		}
+		return parent::__toString();
+	}
+
+}
