1b0ab2d57207601af9258779249d210f796ddabc
syone <syone@localhost>
2011-12-01 17:55:15 +0100

    Update Project Generator: using a template to create Application class

--- syf/Sy/Generator/Project.php
+++ syf/Sy/Generator/Project.php
@@ -18,45 +18,14 @@ class Project {
 	}
 
 	public function generate() {
-		$this->generateApplicationClass();
+		$this->generateFile($this->name . '/Component/Application.php', 'Project/Component/Application.php');
 		$this->generateFile($this->name . '/Component/Application/templates/Application.html', 'Project/Component/Application/templates/Application.html');
+		$this->generateFile($this->name . '/Component/Application/templates/Home.html', 'Project/Component/Application/templates/Home.html');
 		$this->generateFile('index.php');
 		$this->generateFile('index_dev.php');
 		$this->generateFile('conf/conf.php');
 		$this->generateFile('conf/conf.default.php');
 		$this->generateFile('conf/inc.php');
-	}
-
-	private function generateApplicationClass() {
-		$class = new Classe($this->name . '\Component', 'Application');
-		$class->setUsedClasses(array('Sy\Component\WebComponent', 'Sy\Component\Html\Page'));
-		$class->setExtendedClass('Page');
-		$propertyBody = new Property('private', 'body');
-		$class->setProperties(array($propertyBody));
-		$methodConstruct = new Method('public', '__construct');
-		$methodConstruct->setDescription('Application constructor');
-		$methodConstruct->setBody(<<<'EOC'
-		parent::__construct();
-		$this->body = new WebComponent();
-		$this->body->setTemplateFile(__DIR__ . '/Application/templates/Application.html');
-		$this->init();
-		$this->actionDispatch('p', 'home');
-EOC
-		);
-		$methodToString = new Method('public', '__toString');
-		$methodToString->setDescription('Return Application render');
-		$methodToString->setBody(<<<'EOC'
-		$this->setBody($this->body);
-		return parent::__toString();
-EOC
-		);
-		$class->setMethods(array($methodConstruct, $methodToString));
-		$this->generateClass($class);
-	}
-
-	private function generateClass(Classe $class) {
-		$classFile = new ClassFile($this->path, $class);
-		$classFile->generate();
 	}
 
 	private function generateFile($outputFile, $inputFile = NULL) {
--- syf/Sy/Generator/Project/templates/conf/conf.php
+++ syf/Sy/Generator/Project/templates/conf/conf.php
@@ -8,6 +8,17 @@ define('SY_PATH', 'syf');
 // Server path to SyComponents source directory
 define('SYC_PATH', 'syc');
 
+//------------------------------------------------------------------------------
+// Action trigger
+//------------------------------------------------------------------------------
+// The action trigger refers to the method to call from {PROJECT_NAME}\Component\Application
+//------------------------------------------------------------------------------
+// Example:
+// define('ACTION_TRIGGER', 'p');
+// http://my.domain.com/index.php?p=home
+//------------------------------------------------------------------------------
+define('ACTION_TRIGGER', 'p');
+
 // Database informations
 define('HOST'    , 'localhost');
 define('DATABASE', '');
