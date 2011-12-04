3a606dfa4b8ecdf814789747a97ac2b6a34b62e3
Syone <Syone@localhost>
2011-12-04 17:52:25 +0100

    update Project Generator

--- syf/Sy/Generator/Project/templates/Project/Component/Application.php
+++ syf/Sy/Generator/Project/templates/Project/Component/Application.php
@@ -2,8 +2,7 @@
 namespace {PROJECT_NAME}\Component;
 
 use Sy\Component\WebComponent,
-	Sy\Component\Html\Page,
-	Sy\Component\Html\Navigation;
+	Sy\Component\Html\Page;
 
 class Application extends Page {
 
@@ -34,43 +33,14 @@ class Application extends Page {
 	 * Default action
 	 */
 	public function homeAction() {
-		$this->body->setVar('TITLE', '{PROJECT_NAME} - Home Action');
-		$home = new WebComponent();
-		$home->setTemplateFile(__DIR__ . '/Application/templates/Home.html');
-		$this->body->setComponent('CONTENT', $home);
+		$this->body->setVar('PROJECT_NAME', '{PROJECT_NAME}');
 	}
 
 	/**
 	 * Initialize Application
 	 */
 	private function init() {
-		$this->setTitle('{PROJECT_NAME}');
-		$this->initCss();
-		$this->initJs();
-		$this->initNavMenu();
-	}
-
-	/**
-	 * Add CSS links
-	 */
-	private function initCss() {
-		$this->addCssLink('./asset/css/application.css');
-	}
-
-	/**
-	 * Add JS links
-	 */
-	private function initJs() {
-
-	}
-
-	/**
-	 * Create navigation menu
-	 */
-	private function initNavMenu() {
-		$menu = new Navigation();
-		$menu->addItem('Home', $_SERVER['PHP_SELF']);
-		$this->body->setComponent('MENU', $menu);
+		$this->setTitle('Welcome to {PROJECT_NAME}');
 	}
 
 }
