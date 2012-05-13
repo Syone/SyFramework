b2f24fd3885b536b35ca7042760c4675ddac7879
doc <doc@localhost>
2012-05-13 03:11:17 +0200

    Added PhpDoc in ITemplate interface

--- syf/Sy/Template/ITemplate.php
+++ syf/Sy/Template/ITemplate.php
@@ -3,12 +3,34 @@ namespace Sy\Template;
 
 interface ITemplate {
 
+	/**
+	 * Sets a template file
+	 *
+	 * @param string $file
+	 */
 	public function setFile($file);
 
+	/**
+	 * Sets a value for a slot
+	 *
+	 * @param string $var
+	 * @param string $value
+	 * @param bool $append
+	 */
 	public function setVar($var, $value, $append = false);
 
+	/**
+	 * Sets a block
+	 *
+	 * @param string $block
+	 */
 	public function setBlock($block);
 
+	/**
+	 * Returns a template render
+	 *
+	 * @return string
+	 */
 	public function getRender();
 
 }
