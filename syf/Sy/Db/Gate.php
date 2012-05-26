aa7f2aea862adb34a627736d5095a561a727a9e0
syone <syone@localhost>
2012-05-26 03:14:31 +0200

    Add comments

--- syf/Sy/Db/Gate.php
+++ syf/Sy/Db/Gate.php
@@ -19,18 +19,84 @@ class Gate extends \Sy\Object {
 		$this->driverOptions = $driverOptions;
 	}
 
+	/**
+	 * Return debug backtrace informations
+	 *
+	 * @return array
+	 */
+	public function getDebugTrace() {
+		$trace = debug_backtrace();
+		$i = 1;
+		while (isset($trace[$i]) and isset($trace[$i + 1]) and isset($trace[$i + 1]['class']) and ($trace[$i + 1]['class'] === 'Sy\Db')) $i++;
+		$res['class']    = $trace[$i + 1]['class'];
+		$res['function'] = $trace[$i + 1]['function'];
+		$res['file']     = $trace[$i]['file'];
+		$res['line']     = $trace[$i]['line'];
+		return $res;
+	}
+
+	/**
+	 * Return the \PDO object.
+	 *
+	 * @return \PDO
+	 */
+	public function pdo() {
+		if (!isset($this->pdo)) {
+			try {
+				$this->pdo = PDOManager::getPDOInstance($this->dsn, $this->username, $this->password, $this->driverOptions);
+			} catch (\PDOException $except) {
+				$this->logError($except->getMessage());
+			}
+		}
+		return $this->pdo;
+	}
+
+	/**
+	 * Initiates a transaction.
+	 */
 	public function beginTransaction() {
 		return $this->pdo()->beginTransaction();
 	}
 
+	/**
+	 * Commits a transaction.
+	 */
 	public function commit() {
 		return $this->pdo()->commit();
 	}
 
+	/**
+	 * Rolls back a transaction.
+	 */
 	public function rollBack() {
 		return $this->pdo()->rollBack();
 	}
 
+	/**
+	 * Prepares a statement for execution and returns a PDOStatement object.
+	 * Return false on failure.
+	 *
+	 * @param string $sql This must be a valid SQL statement for the target database server.
+	 * @param array $driverOptions This array holds one or more key=>value pairs to set attribute values for the PDOStatement object that this method returns.
+	 * @return PDOStatement
+	 */
+	public function prepare($sql, array $driverOptions = array()) {
+		try {
+			$statement = $this->pdo()->prepare($sql, $driverOptions);
+		} catch (\PDOException $e) {
+			$this->logError($e->getMessage());
+			$statement = false;
+		}
+		return $statement;
+	}
+
+	/**
+	 * Performs a non-query SQL statement, such as INSERT, UPDATE and DELETE.
+	 * It returns the number of rows that are affected by the execution.
+	 *
+	 * @param string | Sy\Db\Sql $sql The SQL query to execute.
+	 * @return int The number of rows affected by the execution.
+	 */
 	public function execute($sql) {
 		$query = $sql;
 		$params = array();
@@ -52,57 +118,13 @@ class Gate extends \Sy\Object {
 	}
 
 	/**
-	 * Return debug backtrace informations
+	 * Performs an SQL statement that returns rows of data, such as SELECT.
+	 * If successful, it returns a PDOStatement instance from which one can traverse the resulting rows of data.
+	 * For convenience, a set of queryXXX() methods are also implemented which directly return the query results.
 	 *
-	 * @return array
+	 * @param string | Sy\Db\Sql $sql The SQL query to execute.
+	 * @return PDOStatement
 	 */
-	public function getDebugTrace() {
-		$trace = debug_backtrace();
-		$i = 1;
-		while (isset($trace[$i]) and isset($trace[$i + 1]) and isset($trace[$i + 1]['class']) and ($trace[$i + 1]['class'] === 'Sy\Db')) $i++;
-		$res['class']    = $trace[$i + 1]['class'];
-		$res['function'] = $trace[$i + 1]['function'];
-		$res['file']     = $trace[$i]['file'];
-		$res['line']     = $trace[$i]['line'];
-		return $res;
-	}
-
-	public function insert($table, array $bind) {
-		$columns = array_keys($bind);
-		$columns = '`' . implode('`,`', $columns) . '`';
-		$values = array_values($bind);
-		$v = array_fill(0, count($bind), '?');
-		$v = implode(',', $v);
-		$sql = new Sql("INSERT INTO $table ($columns) VALUES ($v)", $values);
-		return $this->execute($sql);
-	}
-
-	/**
-	 * Return the \PDO object.
-	 *
-	 * @return \PDO
-	 */
-	public function pdo() {
-		if (!isset($this->pdo)) {
-			try {
-				$this->pdo = PDOManager::getPDOInstance($this->dsn, $this->username, $this->password, $this->driverOptions);
-			} catch (\PDOException $except) {
-				$this->logError($except->getMessage());
-			}
-		}
-		return $this->pdo;
-	}
-
-	public function prepare($sql, array $driverOptions = array()) {
-		try {
-			$statement = $this->pdo()->prepare($sql, $driverOptions);
-		} catch (\PDOException $e) {
-			$this->logError($e->getMessage());
-			$statement = false;
-		}
-		return $statement;
-	}
-
 	public function query($sql) {
 		$query = $sql;
 		$params = array();
@@ -123,6 +145,16 @@ class Gate extends \Sy\Object {
 		return $statement;
 	}
 
+	/**
+	 * Executes the SQL statement and returns all rows.
+	 * See PDOStatement::fetchAll() method documentation for optionnal parameters.
+	 *
+	 * @param string | Sy\Db\Sql $sql The SQL query to execute.
+	 * @param int $fetchStyle Controls the contents of the returned array as documented in PDOStatement::fetch().
+	 * @param mixed $fetchArgs This argument have a different meaning depending on the value of the fetchStyle parameter
+	 * @param array $ctorArgs Arguments of custom class constructor when the fetchStyle parameter is \PDO::FETCH_CLASS
+	 * @return array
+	 */
 	public function queryAll($sql, $fetchStyle = \PDO::FETCH_BOTH, $fetchArgs = NULL, $ctorArgs = array()) {
 		$statement = $this->query($sql);
 		if ($statement === false) return array();
@@ -132,22 +164,68 @@ class Gate extends \Sy\Object {
 			return $statement->fetchAll($fetchStyle, $fetchArgs, $ctorArgs);
 	}
 
+	/**
+	 * Executes the SQL statement and returns the value of a column in the first row of the result.
+	 * This is a convenient method of query when only a single value is needed (e.g. obtaining the count of the records).
+	 * See PDOStatement::fetchColumn() method documentation for optionnal parameter
+	 *
+	 * @param string | Sy\Db\Sql $sql
+	 * @param int $columnNumber
+	 * @return string
+	 */
 	public function queryColumn($sql, $columnNumber = 0) {
 		$statement = $this->query($sql);
 		if ($statement === false) return false;
 		return $statement->fetchColumn($columnNumber);
 	}
 
+	/**
+	 * Executes the SQL statement and returns the first row of the result as an object.
+	 * See PDOStatement::fetchObject() method documentation for optionnal parameters.
+	 *
+	 * @param string | Sy\Db\Sql $sql
+	 * @param string $className Name of the created class.
+	 * @param array $ctorArgs Elements of this array are passed to the constructor.
+	 * @return mixed
+	 */
 	public function queryObject($sql, $className = 'stdClass', array $ctorArgs = array()) {
 		$statement = $this->query($sql);
 		if ($statement === false) return false;
 		return $statement->fetchObject($className, $ctorArgs);
 	}
 
+	/**
+	 * Executes the SQL statement and returns the first row of the result.
+	 * This is a convenient method of query when only the first row of data is needed.
+	 * See PDOStatement::fetch() method documentation for optionnal parameters.
+	 *
+	 * @param string | Sy\Db\Sql $sql The SQL query to execute.
+	 * @param type $fetchStyle Controls how the row will be returned to the caller. This value must be one of the \PDO::FETCH_* constants, defaulting to \PDO::FETCH_BOTH.
+	 * @param type $cursorOrientation This value must be one of the \PDO::FETCH_ORI_* constants, defaulting to \PDO::FETCH_ORI_NEXT.
+	 * @param type $cursorOffset Depends on cursorOrientation value
+	 * @return mixed
+	 */
 	public function queryOne($sql, $fetchStyle = \PDO::FETCH_BOTH, $cursorOrientation = \PDO::FETCH_ORI_NEXT, $cursorOffset = 0) {
 		$statement = $this->query($sql);
 		if ($statement === false) return false;
 		return $statement->fetch($fetchStyle, $cursorOrientation, $cursorOffset);
+	}
+
+	/**
+	 * Insert a table row with specified data.
+	 *
+	 * @param string $table The table name.
+	 * @param array $bind Column-value pairs.
+	 * @return int The number of affected rows.
+	 */
+	public function insert($table, array $bind) {
+		$columns = array_keys($bind);
+		$columns = '`' . implode('`,`', $columns) . '`';
+		$values = array_values($bind);
+		$v = array_fill(0, count($bind), '?');
+		$v = implode(',', $v);
+		$sql = new Sql("INSERT INTO $table ($columns) VALUES ($v)", $values);
+		return $this->execute($sql);
 	}
 
 }
