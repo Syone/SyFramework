92b618a1b595ffb9e41f969e40ea54499d6ebce6
syone <syone@localhost>
2013-03-25 18:46:53 +0100

    New feature in Sy\Db\Sql class: easy writing of a prepared statement using array fro IN clause. See example at https://gist.github.com/Syone/5239052

--- syf/Sy/Db/Sql.php
+++ syf/Sy/Db/Sql.php
@@ -7,17 +7,38 @@ class Sql {
 
 	private $params;
 
+	private $analysedParams;
+
 	public function __construct($sql, $params = array()) {
 		$this->sql = $sql;
 		$this->params = $params;
+		$this->analyse();
 	}
-	
+
 	public function getSql() {
 		return $this->sql;
 	}
-	
+
 	public function getParams() {
 		return $this->params;
+	}
+
+	private function analyse() {
+		if (empty($this->params)) return;
+		if (is_numeric(key($this->params))) return;
+		array_walk($this->params, function($value, $key){
+			if (!is_array($value)) {
+				$this->analysedParams[$key] = $value;
+				return;
+			}
+			$keys = array();
+			foreach ($value as $k => $v) {
+				$this->analysedParams[$key . $k] = $v;
+				$keys[] = $key . $k;
+			}
+			$this->sql = str_replace($key, implode(',', $keys), $this->sql);
+		});
+		$this->params = $this->analysedParams;
 	}
 
 }
