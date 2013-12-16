3aae3c5cff821d55871939ba948ee53697fdb05c
Syone <syone7@gmail.com>
2013-12-16 09:50:31 +0100

    move up method addClass into Html\Element

--- syf/Sy/Component/Html/Form/Element.php
+++ syf/Sy/Component/Html/Form/Element.php
@@ -27,17 +27,6 @@ class Element extends HtmlElement {
 	}
 
 	/**
-	 * Add a class to the element
-	 *
-	 * @param string $class
-	 */
-	public function addClass($class) {
-		$actual = $this->getAttribute('class');
-		$class = is_null($actual) ? $class : $actual . ' ' . $class;
-		$this->setAttribute('class', $class);
-	}
-
-	/**
 	 * Set error message
 	 *
 	 * @param string $error
